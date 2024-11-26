12d5cd714aaf1ce30ec65cbaff2795aac2dfc615
Celal Turkmen <celalsalih64@gmail.com>
2024-11-27 00:06:22 +0300

    webpage v3

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,33 @@
 const navLinks = document.querySelectorAll('nav ul li a');
+
 navLinks.forEach(link => {
   link.addEventListener('click', (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
     document.querySelectorAll('section').forEach(section => {
       section.classList.add('hidden');
     });
     const targetId = link.getAttribute('href').substring(1);
     const targetSection = document.getElementById(targetId);
     targetSection.classList.remove('hidden');
+    history.pushState(null, null, `#${targetId}`);
+    const navUl = document.getElementById('nav-links');
+    const menuIcon = document.getElementById('menu-icon');
+    if (window.innerWidth <= 768 && navUl.style.display === 'flex') {
+      navUl.style.display = 'none';
+      menuIcon.classList.remove('open');
+    }
   });
 });
-document.getElementById('home').classList.remove('hidden');
+
+window.addEventListener('load', () => {
+  const currentHash = window.location.hash.substring(1);
+  const targetSection = document.getElementById(currentHash);
+  if (targetSection) {
+    document.querySelectorAll('section').forEach(section => {
+      section.classList.add('hidden');
+    });
+    targetSection.classList.remove('hidden');
+  } else {
+    document.getElementById('home').classList.remove('hidden');
+  }
+});
